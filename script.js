95ca5fbd18b8946f3f7f7183b6945757c4c2c13d
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-11 19:30:14 +0200

    Add if examples

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 let greetings = "Hello";
 console.log(greetings);
+
+// Structurile condiționale sunt utilizate pentru a executa un bloc de cod în funcție de evaluarea unei expresii sau a unei condiții. Rezultatul evaluării condiției va avea întotdeauna tipul de date boolean (true sau false).
+
+//Principalele structuri condiționale în JavaScript sunt:
+//if
+//if…else
+//operatorul ternar
+//if…else if…else
+//switch
+
+// Conditionals
+
+//if: Este cea  mai simpla structura condițională. Blocul de cod asociat cu ‘if’ este executat doar dacă expresia/condiția este adevărată.
+
+let isChordone = false;
+let hasEaten = false;
+
+if (isChordone && hasEaten) {
+    console.log(`te poti uita la desene`);
+}
+
+if (!isChordone || !hasEaten) {
+
+    if(!isChordone){
+        console.log(`Inca nu te poti uita la desene`);
+    }
+    if(!hasEaten){
+        console.log(`Inca nu ai terminat de mancat, te poti uita la desene cand farfuria e goala`);
+    }
+}
