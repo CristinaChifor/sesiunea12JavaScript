b1d32dcbe626934a245221f4e5d98c23954617a2
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-16 12:55:08 +0200

    Add for of examples

--- script3.js
+++ script3.js
@@ -1,1 +1,16 @@
 console.log(`Hello`);
+
+// for of Este o structura de control repetitiva ce permite executarea unui bloc de cod pentru fiecare valoare dintr-o colecție. 
+
+const numb2 = [22, 11, 91, 7, 33];
+
+let min = numb2[0];
+
+for (const num of numb2) {
+    if (num < min) {
+        min = num;
+    }
+}
+
+console.log(`Cel mai mic numar din array numb2 este: `, min);
+
