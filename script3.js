d9295f7d2ad80938fc4617f20fbd298cc3f0e5fa
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-16 13:15:41 +0200

    Add break and continue examples

--- script3.js
+++ script3.js
@@ -25,3 +25,27 @@ const myCar = {
 for (const property in myCar) {
     console.log(`${property} are valoarea ${myCar[property]}`);
 }
+
+// break Instrucțiunea break este utilizată pentru a întrerupe execuția unei bucle sau a unei instrucțiuni de switch atunci când o anumită condiție este îndeplinită.                                                        
+
+const numb3 = [1, 2, 3, 4, 5, 6];
+const numberToFind2 = 3;
+let isNumbInArray = false;
+
+for (const num of numb3) {
+    console.log(num);
+    if(numberToFind2 === num);
+    isNumbInArray = true;
+    break;
+}
+
+console.log(`${numberToFind2} este in array: ${isNumbInArray}`);
+
+// continue Instrucțiunea continue este folosită pentru a sări la următoarea iterație a unei bucle, ignorând restul codului din blocul de buclă pentru iterația curentă.                                                        
+
+for (const num of numb3) {
+    if (num % 2 > 0) {
+        continue;
+    }
+    console.log(`numarul ${num} este par iar dublul lui este ${num * 2}`);
+}
