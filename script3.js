ac6d896d885ad33fe6987a989d9c1c15bbc2fbaf
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-16 13:00:43 +0200

    Add for in examples

--- script3.js
+++ script3.js
@@ -14,3 +14,14 @@ for (const num of numb2) {
 
 console.log(`Cel mai mic numar din array numb2 este: `, min);
 
+// for ... in Este o structura de control repetitiva  concepută special pentru a parcurge proprietățile unui obiect. Executa un bloc de cod pentru fiecare proprietate.
+
+const myCar = {
+    brand: "Volvo",
+    model: "XC90",
+    manufactureYear: 2012
+};
+
+for (const property in myCar) {
+    console.log(`${property} are valoarea ${myCar[property]}`);
+}
